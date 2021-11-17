328f33761b9d945675574cebb5a1039ec4fa22f5
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-11-17 12:13:35 +0900

    [add] change entity to dto

--- src/main/java/com/java/board/service/user/UserService.java
+++ src/main/java/com/java/board/service/user/UserService.java
@@ -1,8 +1,10 @@
 package com.java.board.service.user;
 
 import com.java.board.domain.entity.User;
+import com.java.board.domain.response.user.UserRo;
 
 public interface UserService {
 	User getUserById(String id);
 	User getUserByIdx(Long id);
+	UserRo userToRo(User user);
 }
--- src/main/java/com/java/board/service/user/UserServiceImpl.java
+++ src/main/java/com/java/board/service/user/UserServiceImpl.java
@@ -2,21 +2,32 @@ package com.java.board.service.user;
 
 import com.java.board.domain.entity.User;
 import com.java.board.domain.repo.UserRepo;
+import com.java.board.domain.response.user.UserRo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 	private final UserRepo userRepo;
 
 	@Override
-	public User getUserById(String id){
+	public User getUserById(String id) {
 		return userRepo.findById(id).orElse(null);
 	}
 
 	@Override
-	public User getUserByIdx(Long idx){
+	public User getUserByIdx(Long idx) {
 		return userRepo.findByIdx(idx).orElse(null);
 	}
+
+	@Override
+	public UserRo userToRo(User user) {
+		return UserRo.builder()
+				.id(user.getIdx())
+				.name(user.getName())
+				.build();
+	}
+
+
 }
