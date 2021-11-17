a3dce4abe3357c2420ccc2258509d1236827342d
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-11-17 12:11:23 +0900

    [update] split board methods

--- src/main/java/com/java/board/service/board/BoardService.java
+++ src/main/java/com/java/board/service/board/BoardService.java
@@ -2,6 +2,7 @@ package com.java.board.service.board;
 
 import com.java.board.domain.dto.board.MakeBoardDto;
 
+import com.java.board.domain.entity.Board;
 import com.java.board.domain.entity.User;
 import com.java.board.domain.response.board.BoardRo;
 
@@ -13,4 +14,7 @@ public interface BoardService {
 	List<BoardRo> getBoards(Long user_idx);
 	void updateBoard(Long board_id, MakeBoardDto makeBoardDto, Long user_idx);
 	void removeBoard(Long board_id, Long user_idx);
+
+	Board findBoardById(Long board_id);
+	Board checkBoardUser(Long board_id, User user);
 }
--- src/main/java/com/java/board/service/board/BoardServiceImpl.java
+++ src/main/java/com/java/board/service/board/BoardServiceImpl.java
@@ -6,11 +6,11 @@ import com.java.board.domain.entity.User;
 import com.java.board.domain.repo.BoardRepo;
 import com.java.board.domain.repo.CommentRepo;
 import com.java.board.domain.repo.LikeRepo;
-import com.java.board.domain.repo.UserRepo;
 import com.java.board.domain.response.board.BoardRo;
 import com.java.board.domain.response.user.UserRo;
 import com.java.board.exception.CustomException;
 import com.java.board.service.user.UserService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -30,11 +29,12 @@ public class BoardServiceImpl implements BoardService {
 	private final UserService userService;
 	private final CommentRepo commentRepo;
 	private final LikeRepo likeRepo;
+
 	@Transactional
 	@Override
 	public BoardRo makeBoard(MakeBoardDto makeBoardDto, Long user_idx) {
 		User user = userService.getUserByIdx(user_idx);
-		log.info(String.valueOf(user));
+
 		Board board = Board.builder()
 				.title(makeBoardDto.getTitle())
 				.content(makeBoardDto.getContent())
@@ -42,27 +42,23 @@ public class BoardServiceImpl implements BoardService {
 		user.add(board);
 
 		BoardRo boardRo = changeBoardToBoardRo(boardRepo.save(board));
-		boardRo.setLikeStatus(false);
-		boardRo.setCommentNum(0L);
-		boardRo.setLikeNum(0L);
+		setBoardLikeAndCommentInfo(boardRo, 0L, 0L, false);
 		return boardRo;
 	}
 
 	@Override
 	public List<BoardRo> getBoards(Long user_idx) {
 		User user = userService.getUserByIdx(user_idx);
-		return boardRepo.findAll(Sort.by(Sort.Direction.DESC,"id")).stream()
-				.map((board)->{
+
+		return boardRepo.findAll(Sort.by(Sort.Direction.DESC, "id")).stream()
+				.map((board) -> {
 					BoardRo boardRo = changeBoardToBoardRo(board);
 
 					Long likeNum = likeRepo.countByBoard(board);
 					Long commentNum = commentRepo.countByBoard(board);
 					Boolean likeStatus = likeRepo.findByBoardAndUser(board, user).isPresent();
 
-					boardRo.setCommentNum(commentNum);
-					boardRo.setLikeNum(likeNum);
-					boardRo.setLikeStatus(likeStatus);
-
+					setBoardLikeAndCommentInfo(boardRo, commentNum, likeNum, likeStatus);
 					return boardRo;
 				})
 				.collect(Collectors.toList());
@@ -72,10 +68,7 @@ public class BoardServiceImpl implements BoardService {
 	@Transactional
 	public void updateBoard(Long board_id, MakeBoardDto makeBoardDto, Long user_idx) {
 		User user = userService.getUserByIdx(user_idx);
-		Board board = boardRepo.findByUserAndId(user, board_id)
-				.orElseGet(()->{
-					throw new CustomException(HttpStatus.NOT_FOUND,"게시글을 찾을 수 없습니다.");
-				});
+		Board board = checkBoardUser(board_id, user);
 		board.setTitle(makeBoardDto.getTitle());
 		board.setContent(makeBoardDto.getContent());
 
@@ -86,20 +79,31 @@ public class BoardServiceImpl implements BoardService {
 	@Transactional
 	public void removeBoard(Long board_id, Long user_idx) {
 		User user = userService.getUserByIdx(user_idx);
-		boardRepo.findByUserAndId(user, board_id)
-				.orElseGet(()->{
-					throw new CustomException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.");
-				});
+		checkBoardUser(board_id, user);
 
 		boardRepo.deleteById(board_id);
 
 	}
 
-	private BoardRo changeBoardToBoardRo(Board board){
-		UserRo user = UserRo.builder()
-				.id(board.getUser().getIdx())
-				.name(board.getUser().getName())
-				.build();
+	@Override
+	public Board findBoardById(Long board_id) {
+		return boardRepo.findById(board_id)
+				.orElseGet(() -> {
+					throw new CustomException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.");
+				});
+	}
+
+	@Override
+	public Board checkBoardUser(Long board_id, User user) {
+		Board board = findBoardById(board_id);
+		if(board.getUser().getIdx().equals(user.getIdx())){
+			return board;
+		}
+		throw new CustomException(HttpStatus.BAD_REQUEST, "유저의 게시글이 아닙니다");
+	}
+
+	private BoardRo changeBoardToBoardRo(Board board) {
+		UserRo user = userService.userToRo(board.getUser());
 		return BoardRo.builder()
 				.id(board.getId())
 				.title(board.getTitle())
@@ -108,4 +112,9 @@ public class BoardServiceImpl implements BoardService {
 				.build();
 	}
 
+	private void setBoardLikeAndCommentInfo(BoardRo boardRo, Long commentNum, Long likeNum, Boolean likeStatus) {
+		boardRo.setCommentNum(commentNum);
+		boardRo.setLikeNum(likeNum);
+		boardRo.setLikeStatus(likeStatus);
+	}
 }
